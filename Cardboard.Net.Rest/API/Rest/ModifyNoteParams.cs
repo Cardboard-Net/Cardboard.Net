f82623ececab52eefcceccd9f495f1c35b1c6d94
puppygirlhornypost <puppygirlhornyposting@gmail.com>
2024-08-29 21:43:15 -0500

    create modify note params, getting ready for ModifyAsync

---
+++ Cardboard.Net.Rest/API/Rest/ModifyNoteParams.cs
@@ -0,0 +1,51 @@
+using Cardboard.Notes;
+using Newtonsoft.Json;
+
+namespace Cardboard.Net.Rest.API;
+
+internal class ModifyNoteParams
+{
+    [JsonProperty("visibility")]
+    public VisibilityType Visibility { get; set; }
+    
+    [JsonProperty("visibileUserIds")]
+    public string[] VisibileUserIds { get; set; }
+
+    [JsonProperty("cw")]
+    public string ContentWarning { get; set; }
+    
+    [JsonProperty("localOnly")]
+    public bool LocalOnly { get; set; }
+    
+    [JsonProperty("reactionAcceptance")]
+    public AcceptanceType ReactionAcceptance { get; set; }
+    
+    [JsonProperty("noExtractMentions")]
+    public bool NoExtractMentions { get; set; }
+    
+    [JsonProperty("noExtractHashtags")]
+    public string NoExtractHashtags { get; set; }
+    
+    [JsonProperty("noExtractEmojis")]
+    public string NoExtractEmojis { get; set; }
+    
+    [JsonProperty("replyId")]
+    public string? ReplyId { get; set; }
+    
+    [JsonProperty("renoteId")]
+    public string? RenoteId { get; set; }
+    
+    [JsonProperty("channelId")]
+    public string? ChannelId { get; set; }
+    
+    [JsonProperty("text")]
+    public string Text { get; set; }
+    
+    [JsonProperty("fileIds")]
+    public string[] FileIds { get; set; }
+    
+    [JsonProperty("mediaIds")]
+    public string[] MediaIds { get; set; }
+
+    // TODO: Poll
+}
